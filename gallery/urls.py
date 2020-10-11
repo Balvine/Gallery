7a16aa8814c63a277144aaad6cfb36cf95cb3e6b
Balvine <balvy075@gmail.com>
2020-10-11 11:11:21 +0300

    added settings

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url()
+    # landing page
+    url('^$', views.photos, name='photos'),
 ]
