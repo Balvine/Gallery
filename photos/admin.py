11bcc8eb0c4dccb4a5cf91cc3b3144f0c381e547
Balvine <balvy075@gmail.com>
2020-10-11 15:05:32 +0300

    added admin site

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from .models import Location,Category,Image
+from django.contrib import admin
+from .models import Location,Category,Image
 
-# # image details
-# admin.site.register(Location)
-# admin.site.register(Image)
-# admin.site.register(Category)
+# image details
+admin.site.register(Location)
+admin.site.register(Image)
+admin.site.register(Category)
