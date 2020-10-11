7fd5830a6428bef0edc47447103f4d02a80a7c8b
Balvine <balvy075@gmail.com>
2020-10-11 15:36:41 +0300

    added templates file

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse,Http404
-# from .models import Image
+from .models import Image
 
 # Create your views here.
 def photos(request):
